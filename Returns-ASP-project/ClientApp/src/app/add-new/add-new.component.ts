d3aebc572a64441a7d56180696b0c7bbcdd93038
Rowan Abrahams <rabrahams65@gmail.com>
2023-10-16 12:31:28 +0200

    LFS init

--- Returns-ASP-project/ClientApp/src/app/add-new/add-new.component.ts
+++ Returns-ASP-project/ClientApp/src/app/add-new/add-new.component.ts
@@ -108,8 +108,8 @@ export class AddNewComponent implements OnInit {
     qtyOnDoc: [0],
     qtyReturned: [0],
     batchDate: [],
-    owner: [{ id: '', ownerName: '' }],
-    fault: [{ id: '', faultName: '' }],
+    owner: [{ id: '', firstName: '' }],
+    fault: [{ id: '', name: '' }],
     docNo: ['', Validators.required],
     docDate: [,Validators.required],
     resolved: [],
@@ -165,38 +165,48 @@ export class AddNewComponent implements OnInit {
       })
 
     }
+    if (type == 'ownerType') {
+      this.ownerIdFromTemplate = ''
+      this.ownerList = []
+      this.ownerService.searchOwner().subscribe(o => {
+        this.ownerList = o
+        console.log('2. searchowner subscribe call done')
 
-    //if (type == 'productType') {
-    //  this.prodIdFromTemplate = ''
-    //  let productFound = this.productList.filter(p => p.productName?.toLowerCase().trim() == modelName?.toLowerCase().trim())
-    //  if (productFound.length > 0) {
-    //    productFound.map(p => { this.prodIdFromTemplate = p.id!; this.product.productName = p.productName! })
-    //  }
-    //  else {
-    //    this.prodIdFromTemplate = this.notFound
-    //  }
-    //}
-    //if (type == 'faultType') {
-    //  this.faultIdFromTemplate = ''
-    //  let faultFound = this.faultList.filter(f => f.name?.toLowerCase().trim() == modelName?.toLowerCase().trim())
-    //  if (faultFound.length > 0) {
-    //    faultFound.map(f => { this.faultIdFromTemplate = f.id!; this.fault.name = f.name! })
-    //  }
-    //  else {
-    //    this.faultIdFromTemplate = this.notFound
-    //  }
-    //}
-    //if (type == 'ownerType') {
-    //  this.ownerIdFromTemplate = ''
-    //  let ownerFound = this.ownerList.filter(o => o.firstName?.toLowerCase().trim() == modelName?.toLowerCase().trim())
-    //  if (ownerFound.length > 0) {
-    //    ownerFound.map(o => { this.ownerIdFromTemplate = o.id!; this.owner.firstName = o.firstName! })
-    //  }
-    //  else {
-    //    this.ownerIdFromTemplate = this.notFound
-    //  }
-    //}
+        let ownerFound = this.ownerList.filter(o => o.firstName?.toLowerCase().trim() == modelName?.toLowerCase().trim())
+        console.log('3. owner found length: ' + ownerFound.length)
 
+        if (ownerFound.length > 0) {
+          ownerFound.map(o => { this.ownerIdFromTemplate = o.id!; this.owner.firstName = o.firstName! })
+          console.log('4.Get model gets called- ownerIdfromTemplate: {' + this.ownerIdFromTemplate + '} model name: ' + this.owner.firstName)
+        }
+        else if (ownerFound.length <= 0) {
+          this.ownerIdFromTemplate = this.notFound
+        }
+
+      })
+
+    }
+    if (type == 'faultType') {
+      this.faultIdFromTemplate = ''
+      this.faultList = []
+      this.faultService.searchFault().subscribe(f => {
+        this.faultList = f
+        console.log('2. searchfault subscribe call done')
+
+        let faultFound = this.faultList.filter(f => f.name?.toLowerCase().trim() == modelName?.toLowerCase().trim())
+        console.log('3. fault found length: ' + faultFound.length)
+
+        if (faultFound.length > 0) {
+          faultFound.map(c => { this.faultIdFromTemplate = c.id!; this.fault.name = c.name! })
+          console.log('4.Get model gets called- custIdfromTemplate: {' + this.faultIdFromTemplate + '} model name: ' + modelName)
+        }
+        else if (faultFound.length <= 0) {
+          this.faultIdFromTemplate = this.notFound
+        }
+
+      })
+
+    }
 
   }
 
@@ -244,11 +254,11 @@ export class AddNewComponent implements OnInit {
       this.getProduct.invalid
       this.prodIdFromTemplate = 'none'
     }
-    if (this.getFault.value?.faultName == undefined || this.getFault.value?.faultName == '' || this.getFault.value?.id == this.notFound) {
+    if (this.getFault.value?.name == undefined || this.getFault.value?.name == '' || this.getFault.value?.id == this.notFound) {
       this.getFault.invalid
       this.faultIdFromTemplate = 'none'
     }
-    if (this.getOwner.value?.ownerName == undefined || this.getOwner.value?.ownerName == '' || this.getOwner.value?.id == this.notFound) {
+    if (this.getOwner.value?.firstName == undefined || this.getOwner.value?.firstName == '' || this.getOwner.value?.id == this.notFound) {
       this.getOwner.invalid
       this.ownerIdFromTemplate = 'none'
     }
@@ -327,6 +337,16 @@ export class AddNewComponent implements OnInit {
     shortCode: [''],
   })
 
+  newOwnerForm = this.fb.group({
+    ownerFirstName: ['', Validators.required],
+    ownerLastName: ['']
+  })
+
+  newFaultForm = this.fb.group({
+    faultName: ['', Validators.required],
+    description: ['']
+  })
+
   //
 
   //Modal Properties
@@ -340,7 +360,12 @@ export class AddNewComponent implements OnInit {
   productAlreadyExists = 'Product already exists'
   productShortCodeAlreadyExists = 'Short code already exists'
 
-  //customer Modal
+  faultIdFromModal = ''
+  faultAlreadyExists = 'Fault already exists'
+
+  ownerIdFromModal = ''
+
+  //Open Modal
   openCustomerModal(content: any) {
     this.newCustomerForm.reset()
     this.customerIdFromModal = ''
@@ -374,6 +399,35 @@ export class AddNewComponent implements OnInit {
     );
   }
 
+  openOwnerModal(content: any) {
+    this.newOwnerForm.reset()
+    this.ownerIdFromModal = ''
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+        this.saveNewOwner()
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
+
+  openFaultModal(content: any) {
+    this.newFaultForm.reset()
+    this.faultIdFromModal = ''
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+        this.saveNewfault()
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
 
   //modal save methods
 
@@ -435,6 +489,57 @@ export class AddNewComponent implements OnInit {
     })
 
   }
+  saveNewOwner(): void {
+    
+    let newOwner: OwnerRm = {
+      firstName: this.newOwnerForm.controls.ownerFirstName.value?.trim(),
+      lastName: this.newOwnerForm.controls.ownerLastName.value?.trim()
+    }
+
+    this.ownerService.createOwner({ body: newOwner }).subscribe(c => {
+      //this.showToast = true
+      //this.message = 'owner added successfully'
+      console.log('1. Saved, then getModel gest called')
+      this.getModel(newOwner.firstName!, 'ownerType')
+      console.log('5. Trying to add new owner:' + newOwner.firstName! +' to form')
+      this.form.controls.owner.setValue({ id: this.ownerIdFromTemplate, firstName: newOwner.firstName! })
+      //else if (this.newownerForm.valid && !this.form.touched && !this.form.dirty) {
+      //  this.message = 'Nothing added'
+      //  console.log('Nothing added')
+      //}
+      //this.appService.setMessage(this.message);
+      //this.appService.showToast(this.showToast)
+      console.log('6. Trying to set owner {' + newOwner.firstName + '} The id is: {' + this.ownerIdFromTemplate + '}')
+    })
+
+  }
+  saveNewfault(): void {
+    if (this.faultIdFromModal == this.faultAlreadyExists) {
+      return
+    }
+
+    let newFault: FaultRm = {
+      name: this.newFaultForm.controls.faultName.value?.trim(),
+      description: this.newFaultForm.controls.description.value?.trim()
+    }
+
+    this.faultService.createFault({ body: newFault }).subscribe(c => {
+      //this.showToast = true
+      //this.message = 'fault added successfully'
+      console.log('1. Saved, then getModel gest called')
+      this.getModel(newFault.name!, 'faultType')
+      console.log('5. Trying to add fault to form')
+      this.form.controls.fault.setValue({ id: this.faultIdFromTemplate, name: newFault.name! })
+      //else if (this.newfaultForm.valid && !this.form.touched && !this.form.dirty) {
+      //  this.message = 'Nothing added'
+      //  console.log('Nothing added')
+      //}
+      //this.appService.setMessage(this.message);
+      //this.appService.showToast(this.showToast)
+      console.log('6. Trying to set fault {' + newFault.name + '} The id is: {' + this.faultIdFromTemplate + '}')
+    })
+
+  }
 
   //Modal extension Methods
 
@@ -481,6 +586,14 @@ export class AddNewComponent implements OnInit {
       if (skuFound.length > 0) {
         this.productShortCodeFromTemplate = this.productShortCodeAlreadyExists
       }
+    }
+  }
+
+  faultExists(faultName: string) {
+    this.faultIdFromModal = ''
+    let faultFound = this.faultList.filter(f => f.name?.toLowerCase().trim() == faultName.toLowerCase().trim())
+    if (faultFound.length > 0) {
+      this.faultIdFromModal = this.faultAlreadyExists
     }
   }
 }
--- Returns-ASP-project/ClientApp/src/app/customers/customers.component.ts
+++ Returns-ASP-project/ClientApp/src/app/customers/customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CustomerRm } from '../api/models';
 import { CustomerService } from '../api/services';
 
@@ -10,17 +11,81 @@ import { CustomerService } from '../api/services';
 export class CustomersComponent implements OnInit {
 
   customerList: CustomerRm[] = []
+  closeResult = ''
+  page = 1
+  pageSize = 10
+  collectionSize = 0
+  numberOfResults = 0
+  prevPhrase = ''
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
-    this.customerService.searchCustomer({}).subscribe(c => this.customerList = c,this.handleError)
+    this.getAllCustomers()
+  }
+
+  private getAllCustomers() {
+    this.customerService.searchCustomer({}).subscribe(c => {
+      this.customerList = c
+      this.collectionSize = c.length
+      this.numberOfResults = c.length
+    }, this.handleError)
   }
 
   private handleError(err: any) {
     console.log("Response Error. Status: ", err.status)
     console.log("Response Error. Status: ", err.statusText)
     console.log(err);
+  }
+
+  deleteCustomer(customerToDeleteId: string) {
+
+    this.customerService.deleteCustomer({ id: customerToDeleteId }).subscribe(c => {
+      this.customerList = this.customerList.filter(c => {
+        c.id != customerToDeleteId
+        this.collectionSize = this.customerList.length
+        this.numberOfResults = this.customerList.length
+      });
+      
+    }, this.handleError)
+  }
+
+  openDeleteCustModal(content: any, deleteCustomerId: string) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+        this.deleteCustomer(deleteCustomerId)
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  search(phrase: string) {
+
+    if (phrase == '' || (phrase != this.prevPhrase && this.prevPhrase != '')) {
+      this.getAllCustomers()
+      console.log(`First IF statement runs. Phrase = ${phrase} and prevPhrase = ${this.prevPhrase}`)
+    }
+    if (phrase.length > 0 && phrase != '') {
+      console.log("Search runs with: " + phrase)
+      this.customerList = this.customerList.filter(c => c.customerName?.toLowerCase().includes(phrase.toLowerCase().trim()) || c.shortCode?.toLowerCase().trim().includes(phrase.toLowerCase().trim()))
+      this.collectionSize = this.customerList.length
+      this.numberOfResults = this.customerList.length
+      console.log(`Result: ${this.customerList}`)
+      this.prevPhrase = phrase
+    }
   }
 
 }
